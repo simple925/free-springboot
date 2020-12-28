0258cd2144c650599b40bc555565ad91a501795e
simple925 <simple925@naver.com>
2020-12-28 14:31:45 +0900

    spring git commit 연습 20.12.28.

--- src/main/java/com/free_project/book/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/free_project/book/springboot/config/auth/CustomOAuth2UserService.java
@@ -44,10 +44,10 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
     }
 
     private  User saveOrUpdate(OAuthAttributes attributes){
-//        User user = userRepository.findByEmail(attributes.getEmail())
-//                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
-//                .orElse(attributes.toEntity());
-//        return userRepository.save(user);
-        return null;
+        User user = userRepository.findByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                .orElse(attributes.toEntity());
+        return userRepository.save(user);
+//        return null;
     }
 }
